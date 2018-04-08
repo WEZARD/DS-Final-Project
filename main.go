8732dbe56235f3c6d133f8a82ab0a858aa02a088
Yizhou Chen <yc2755@nyu.edu>
2018-04-08 16:48:54 -0400

    Implement homeHandler func and create default users

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
     "fmt"
     "log"
+    "sort"
     "net/http"
     //"container/list"
     "html/template"
@@ -17,14 +18,23 @@ import (
 type User struct { // capitalize the first letter of each field name as "exported" for gob 
     Username string
     Password string
-    Following map[string]struct{}
-    Follower map[string]struct{}
+    Following map[string]bool
+    Follower map[string]bool
     Messages map[time.Time]string
 }
 
 type Message struct {
-    timestamp time.Time
-    text string
+    Username string
+    Timestamp time.Time
+    Text string
+    DisplayTime string
+}
+
+type MessageBox struct {
+    Username string
+    Following map[string]bool
+    Follower map[string]bool
+    Messages []Message
 }
 
 // registerHandler
@@ -37,17 +47,69 @@ func registerHandler(response http.ResponseWriter, request *http.Request) {
         request.ParseForm()
         fmt.Println("username:", request.Form["username"])
         mc := memcache.New("127.0.0.1:11211") // brew services restart memcached
+        
+        // ----------------------------------------Test Case---------------------------------------------
+        testTime1 := time.Date(2018, 4, 4, 20, 00, 00, 651387237, time.UTC)
+        testTime2 := time.Date(2018, 4, 5, 20, 00, 00, 651387237, time.UTC)
+        messages := map[time.Time]string {testTime1: "Hello World!", testTime2: "Welcome to our web service!"}
+
+        testUser1 := User {
+            Username: "Thierry",
+            Messages: messages,
+        }
+
+        testUser2 := User {
+            Username: "Ramsey",
+            Messages: messages,
+        }
+
+        _, memErr := mc.Get("Thierry")
+        
+        if memErr == nil {
+
+        } else {
+            fmt.Println(memErr)
+            // Encode struct to byte array
+            encBuf := new(bytes.Buffer)
+            encErr := gob.NewEncoder(encBuf).Encode(testUser1)
+            if encErr != nil {
+                log.Fatal(encErr)
+            }
+            value := encBuf.Bytes()
+            mc.Set(&memcache.Item{Key: "Thierry", Value: []byte(value)}) // store in memcache
+        }
+
+        _, memErr = mc.Get("Ramsey")
+        
+        if memErr == nil {
+
+        } else {
+            fmt.Println(memErr)
+            // Encode struct to byte array
+            encBuf := new(bytes.Buffer)
+            encErr := gob.NewEncoder(encBuf).Encode(testUser2)
+            if encErr != nil {
+                log.Fatal(encErr)
+            }
+            value := encBuf.Bytes()
+            mc.Set(&memcache.Item{Key: "Ramsey", Value: []byte(value)}) // store in memcache
+        }
+
+        // ----------------------------------------------------------------------------------------------
+        
         username := request.FormValue("username")
         password := request.FormValue("password")
-        following := make(map[string]struct{})
-        follower := make(map[string]struct{})
+        following := map[string]bool {testUser1.Username: true, testUser2.Username: true}
+        follower := map[string]bool {testUser1.Username: true, testUser2.Username: true}
+        //following := make(map[string]bool)
+        //follower := make(map[string]bool)
         user := User {
             Username: username,
             Password: password,
             Following: following,
             Follower: follower,
         }
-        _, memErr := mc.Get(username)
+        _, memErr = mc.Get(username)
         
         if memErr == nil {
             fmt.Fprintf(response, "<script>alert('this username has been registered')</script>")
@@ -55,14 +117,14 @@ func registerHandler(response http.ResponseWriter, request *http.Request) {
             t.Execute(response, nil)
         } else {
             fmt.Println(memErr)
-            //TODO  Encode struct to byte array
+            // Encode struct to byte array
             encBuf := new(bytes.Buffer)
             encErr := gob.NewEncoder(encBuf).Encode(user)
             if encErr != nil {
                 log.Fatal(encErr)
             }
             value := encBuf.Bytes()
-            mc.Set(&memcache.Item{Key: username, Value: []byte(value)})
+            mc.Set(&memcache.Item{Key: username, Value: []byte(value)}) // store in memcache
             setSession(username, response)    
             http.Redirect(response, request, "/home", http.StatusSeeOther)
         }
@@ -82,7 +144,7 @@ func loginHandler(response http.ResponseWriter, request *http.Request) {
         password := request.FormValue("password")
 
         val, memErr := mc.Get(username)
-        //TODO  Decode byte array to struct
+        // Decode byte array to struct
         decBuf := bytes.NewBuffer(val.Value)
         userOut := User{}
         decErr := gob.NewDecoder(decBuf).Decode(&userOut)
@@ -126,14 +188,34 @@ func setSession(userName string, response http.ResponseWriter) {
     }
 }
 
-func getUserName(request *http.Request) (userName string) {
+func getUserName(request *http.Request) (username string) {
     if cookie, err := request.Cookie("session"); err == nil {
         cookieValue := make(map[string]string)
         if err = cookieHandler.Decode("session", cookie.Value, &cookieValue); err == nil {
-            userName = cookieValue["name"]
+            username = cookieValue["name"]
         }
     }
-    return userName
+    return username
+}
+
+func getUserInfo(request *http.Request) *User {
+    if cookie, err := request.Cookie("session"); err == nil {
+        cookieValue := make(map[string]string)
+        if err = cookieHandler.Decode("session", cookie.Value, &cookieValue); err == nil {
+            username := cookieValue["name"]
+            mc := memcache.New("127.0.0.1:11211")
+            val, _ := mc.Get(username)
+            // Decode byte array to struct
+            decBuf := bytes.NewBuffer(val.Value)
+            userOut := User{}
+            decErr := gob.NewDecoder(decBuf).Decode(&userOut)
+            if decErr != nil {
+                log.Fatal(decErr)
+            }
+            return &userOut
+        }
+    }
+    return nil   
 }
 
 func clearSession(response http.ResponseWriter) {
@@ -150,11 +232,55 @@ func clearSession(response http.ResponseWriter) {
 func homeHandler(response http.ResponseWriter, request *http.Request) {
     if request.Method == "GET" {
         t, _ := template.ParseFiles("home.gtpl")
-        t.Execute(response, nil)
-        //username := getUserName(request)
-        //fmt.Fprintf(response, "This is a test for the broadcasting system %s!", username)
+        
+        mc := memcache.New("127.0.0.1:11211")
 
+        userInfo := getUserInfo(request)
+        fmt.Printf("This is a test for %s\n", userInfo.Username)
+        
+        messageBox := MessageBox{}
+        messages := []Message{}
+        singleMessage := Message{}
+        
+        for k := range userInfo.Following {
+            fmt.Printf("%s\n", k)
+            val, memErr := mc.Get(k)
+            if memErr == nil {
+                decBuf := bytes.NewBuffer(val.Value)
+                userOut := User{}
+                decErr := gob.NewDecoder(decBuf).Decode(&userOut)
+                if decErr != nil {
+                    log.Fatal(decErr)
+                }
+                for k1, v1 := range userOut.Messages {
+                    singleMessage.Username = k
+                    singleMessage.Timestamp = k1
+                    singleMessage.Text = v1
+                    messages = append(messages, singleMessage)
+                }
+
+            } else {
+                log.Fatal(memErr)
+            }
+
+        }
+
+        sort.Slice(messages, func(i, j int) bool { 
+            return messages[i].Timestamp.After(messages[j].Timestamp)
+        })
+
+        for i := 0; i < len(messages); i++ {
+            messages[i].DisplayTime = messages[i].Timestamp.Format("Mon Jan _2 15:04:05 2006")
+        }
+
+        messageBox.Username = userInfo.Username
+        messageBox.Following = userInfo.Following
+        messageBox.Follower = userInfo.Follower
+        messageBox.Messages = messages
+
+        t.Execute(response, messageBox)
     }
+
     if request.Method == "POST" {
         t, _ := template.ParseFiles("home.gtpl")
         t.Execute(response, nil)
@@ -189,17 +315,9 @@ func handler(w http.ResponseWriter, r *http.Request) {
 var router = mux.NewRouter()
 
 func main() {
-    mc := memcache.New("127.0.0.1:11211")
-    mc.Set(&memcache.Item{Key: "key_one", Value: []byte("michael")})
-    mc.Set(&memcache.Item{Key: "key_two", Value: []byte("programming")})
-    val, err := mc.Get("key_one")
+    //mc := memcache.New("127.0.0.1:11211")
+    fmt.Println("Welcome to message web application!")
 
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-
-    fmt.Printf("-- %s", val.Value)
     http.Handle("/", router)
     router.HandleFunc("/", handler)
     router.HandleFunc("/register", registerHandler)
